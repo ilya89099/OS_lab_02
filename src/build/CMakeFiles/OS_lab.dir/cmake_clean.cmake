f2fb6d7003362131c21da97c69e4a1fdb269e64e
ilya <ilya.semenov89099@yandex.ru>
2019-10-18 23:11:16 +0300

    build

---
+++ src/build/CMakeFiles/OS_lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_lab.dir/main.c.o"
+  "OS_lab.pdb"
+  "OS_lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
